2bdf90a6dd45b899b54d50e895128d0a01112c95
fahadAD <mrfahad26531@gmail.com>
2022-12-31 21:57:56 +0600

    first commit

---
+++ app/src/main/java/com/f_s_news/hello/MainActivity.java
@@ -0,0 +1,19 @@
+package com.f_s_news.hello;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import soup.neumorphism.NeumorphButton;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    }
---
+++ app/src/main/java/com/f_s_news/hello/MainActivity2.java
@@ -0,0 +1,21 @@
+package com.f_s_news.hello;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+
+
+
+
+    }
+}
